bab991dbd6b0e1b0cd9b8f807e8836136328402a
unknown <elfabnyl@gmail.com>
2022-04-04 02:19:10 +0100

    google maps config

--- lib/presentation/Screens/send/delivery_details/delivery_details_controller.dart
+++ lib/presentation/Screens/send/delivery_details/delivery_details_controller.dart
@@ -6,10 +6,4 @@ class DeliveryDetailsController extends GetxController {
 
 
 
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);  
 }
